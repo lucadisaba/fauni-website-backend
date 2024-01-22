3be6b7d04927fe145b9d39aca46aaf2253e2ede2
Jessica Leone <jessica.leone@seidorgunpowder.com@MacBook-Pro-di-Jessica.local>
2024-01-22 16:11:13 +0100

    getAllUsers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
 
-    constructor(private userService: UserService) {}
+  @Post()
+  addUser(@Body() createUserDto: CreateUserDto): Promise<any> {
+    return this.userService.addUser(createUserDto);
+  }
 
-    @Post() 
-    addUser(@Body() createUserDto: CreateUserDto): Promise<any> {
-        return this.userService.addUser(createUserDto);
-    }
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
 }
