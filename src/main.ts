01275b3e13f992d77b8b9234c5d4df6210604fde
lucadisaba <lucadisabatino97@gmail.com>
2024-01-24 14:14:22 +0100

    Enabled CORS policies

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(
     cookieSession({
       keys: ['asdfasdf'],
